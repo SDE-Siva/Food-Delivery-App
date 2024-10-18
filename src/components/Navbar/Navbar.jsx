f08edeab2edc7d38a5aed9f9d76c845c301eea11
SDE-Siva <journeyofsiva@gmail.com>
2024-10-18 18:55:53 +0530

    React router is installed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="nav-bar">
+        <img src={assets.log} alt="" />
+        <ul className="navbar-menu">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Mobile App</li>
+          <li>Contact</li>
+        </ul>
+        <div className="navbar-right">
+          <img src={assets.search_icon} alt="" />
+          <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+          </div>
+          <button>Sign up</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;

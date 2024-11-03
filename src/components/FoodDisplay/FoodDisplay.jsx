31f7bc5bc8153cf030613855a06f8c38454ab4a3
SDE-Siva <journeyofsiva@gmail.com>
2024-11-03 23:14:23 +0530

    Feat: Menu update

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../components/Context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = () => {
   const { food_list } = useContext(StoreContext);
@@ -8,6 +9,20 @@ const FoodDisplay = () => {
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              id={item._id}
+              price={item.price}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
